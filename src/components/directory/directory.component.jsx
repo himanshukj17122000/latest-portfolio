ab83fabc6d028788fca6ea34ac5eb3a4ef5ac6ed
Himanshu Jain <hkj4@dj4.edu>
2020-05-01 04:00:53 +0530

    Made the homepage better

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,32 +14,25 @@ class Directory extends React.Component {
           title: "",
           imageUrl: "https://i.ibb.co/vmcYqLp/DSC1502.jpg",
           size: "name",
-          info: `
-          Hi! My name is Himanshu Jain. I am a rising Junior at
-          Duke University, North Carolina
-          studying Electrical & Computer Engineering-Computer Science. I am also planning to complete a minor in Financial Economics.
-  
-    
-          My home town is New Delhi, India
-          but I currently live in Durham,
-          North Carolina. I have deep interest in applying Computer Science in
-          other fields like Finance and Health. For almost a year, I worked on an
-          Android Application named Digital Fidget. I have also worked on a bot
-          for excavating and mapping archaeological sites. Please head over to my
-          Portfolio page for more information.
-        
-          I am a fitness enthusiast and I love to hit the gym in my freetime. I
-          also play sports like Tennis and Basketball (Let's go Devils!).
-  
-        
-         
-          `,
+          name: "profile",
+          info: `Hey! My name is Himanshu Jain`,
+          id: 1,
+          linkUrl: "about",
+        },
+        {
+          title: "About Me",
+          imageUrl: "https://i.ibb.co/nrtYZng/city.jpg",
+          size: "name",
+          name: "info",
+          info:
+            "Hi! My name is Himanshu Jain. I am a rising Junior at Duke University, North Carolina studying Electrical & Computer Engineering-Computer Science. I am also planning to complete a minor in Financial Economics. My home town is New Delhi, India but I currently live in Durham, North Carolina. I have deep interest in applying Computer Science in other fields like Finance and Health. For almost a year, I worked on an Android Application named Digital Fidget. I have also worked on a bot for excavating and mapping archaeological sites. Please head over to my Portfolio page for more information. I am a fitness enthusiast and I love to hit the gym in my freetime. I also play sports like Tennis and Basketball (Let's go Devils!).",
           id: 1,
           linkUrl: "about",
         },
         {
           title: "Resume",
-          size: "name",
+          size: "",
+          info: `Linked to Google Drive`,
           imageUrl:
             "https://i.ibb.co/vQ79B4Q/Screenshot-2020-04-28-at-10-34-06-PM.png",
           id: 2,
@@ -48,6 +41,7 @@ class Directory extends React.Component {
         },
         {
           title: "Work Experience",
+          info: `Projects I've worked on`,
           imageUrl:
             "https://i.ibb.co/qWLgrfc/Screenshot-2020-04-28-at-10-39-40-PM.png",
           id: 3,
@@ -58,7 +52,7 @@ class Directory extends React.Component {
           title: "Contact Me",
           imageUrl: "https://i.ibb.co/tq1cgbx/Unknown.jpg",
           id: 5,
-          info: `himanshu.jain@duke.edu`,
+          info: "himanshu.jain@duke.edu",
           linkUrl: "mailto:hkj4@duke.edu",
         },
       ],
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,31 +1,69 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-
+import Typewriter from "typewriter-effect";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, info, imageUrl, size, history, linkUrl, match }) => (
-  <div
-    className={`${size} menu-item`}
-    onClick={() => window.location.replace(`${linkUrl}`)}
-  >
+const MenuItem = ({
+  title,
+  info,
+  imageUrl,
+  size,
+  name,
+  history,
+  linkUrl,
+  match,
+}) =>
+  name !== "profile" ? (
     <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    {size === "name" ? (
-      <div className='name'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>{info}</span>
-      </div>
-    ) : (
-      <div className='content'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>{info}</span>
-      </div>
-    )}
-  </div>
-);
+      className={`${size} menu-item`}
+      onClick={() => window.location.replace(`${linkUrl}`)}
+    >
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      {size === "name" && title.trim() ? (
+        <div className={`${name} name`}>
+          <h1 className='title'>{title.toUpperCase()}</h1>
+          <span className='subtitle'>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(" Hi! My name is Himanshu Jain. ")
+                  .pasteString(
+                    "I am a rising Junior at Duke University, North Carolina studying Electrical & Computer Engineering-Computer Science. I am also planning to complete a minor in Financial Economics. "
+                  )
+                  .typeString("Click here to read more!")
+                  .pauseFor(2500)
+                  .callFunction(() => {
+                    console.log("All strings were deleted");
+                  })
+                  .start();
+              }}
+            />
+          </span>
+        </div>
+      ) : (
+        <div className='content'>
+          <h1 className='title'>{title.toUpperCase()}</h1>
+          <span className='subtitle'>{info}</span>
+        </div>
+      )}
+    </div>
+  ) : (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => window.location.replace(`${linkUrl}`)}
+    >
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+    </div>
+  );
 
 export default withRouter(MenuItem);
