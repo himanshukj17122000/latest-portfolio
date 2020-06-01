b080c6d683c727a64dc0c1d45864dcb77ffcc294
Himanshu Jain <hkj4@dj4.edu>
2020-06-01 17:05:12 -0500

    resume link changed "

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import MenuItem from "../menu-item/menu-item.component";
+import MenuItem from '../menu-item/menu-item.component';
 
-import "./directory.styles.scss";
+import './directory.styles.scss';
 
 class Directory extends React.Component {
   constructor() {
@@ -11,49 +11,49 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: "",
-          imageUrl: "https://i.ibb.co/3RNdbqY/IMG-2826.jpg",
-          size: "name",
-          name: "profile",
+          title: '',
+          imageUrl: 'https://i.ibb.co/3RNdbqY/IMG-2826.jpg',
+          size: 'name',
+          name: 'profile',
           info: `Hey! My name is Himanshu Jain`,
           id: 1,
-          linkUrl: "about",
+          linkUrl: 'about',
         },
         {
-          title: "About Me",
-          imageUrl: "https://i.ibb.co/nrtYZng/city.jpg",
-          size: "name",
-          name: "info",
+          title: 'About Me',
+          imageUrl: 'https://i.ibb.co/nrtYZng/city.jpg',
+          size: 'name',
+          name: 'info',
           info:
             "Hi! My name is Himanshu Jain. I am a rising Junior at Duke University, North Carolina studying Electrical & Computer Engineering-Computer Science. I am also planning to complete a minor in Financial Economics. My home town is New Delhi, India but I currently live in Durham, North Carolina. I have deep interest in applying Computer Science in other fields like Finance and Health. For almost a year, I worked on an Android Application named Digital Fidget. I have also worked on a bot for excavating and mapping archaeological sites. Please head over to my Portfolio page for more information. I am a fitness enthusiast and I love to hit the gym in my freetime. I also play sports like Tennis and Basketball (Let's go Devils!).",
           id: 1,
-          linkUrl: "about",
+          linkUrl: 'about',
         },
         {
-          title: "Resume",
-          size: "",
+          title: 'Resume',
+          size: '',
           info: `Linked to Google Drive`,
           imageUrl:
-            "https://i.ibb.co/vQ79B4Q/Screenshot-2020-04-28-at-10-34-06-PM.png",
+            'https://i.ibb.co/vQ79B4Q/Screenshot-2020-04-28-at-10-34-06-PM.png',
           id: 2,
           linkUrl:
-            "https://drive.google.com/file/d/1QU6RULb4XnTNtTvzVqz-fCwW8VtGJ3FY/view",
+            'https://drive.google.com/file/d/1irMGuvR4sFj90a4uPeAiWLUrBYzK8BZb/view?usp=sharing',
         },
         {
-          title: "Work Experience",
+          title: 'Work Experience',
           info: `Projects I've worked on`,
           imageUrl:
-            "https://i.ibb.co/qWLgrfc/Screenshot-2020-04-28-at-10-39-40-PM.png",
+            'https://i.ibb.co/qWLgrfc/Screenshot-2020-04-28-at-10-39-40-PM.png',
           id: 3,
-          linkUrl: "projects",
+          linkUrl: 'projects',
         },
 
         {
-          title: "Contact Me",
-          imageUrl: "https://i.ibb.co/tq1cgbx/Unknown.jpg",
+          title: 'Contact Me',
+          imageUrl: 'https://i.ibb.co/tq1cgbx/Unknown.jpg',
           id: 5,
-          info: "himanshu.jain@duke.edu",
-          linkUrl: "mailto:hkj4@duke.edu",
+          info: 'himanshu.jain@duke.edu',
+          linkUrl: 'mailto:hkj4@duke.edu',
         },
       ],
     };
